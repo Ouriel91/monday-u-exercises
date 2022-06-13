6768c28655d074266ea5a42b46da25aa230188d6
Ouriel91 <orieloh91@gmail.com>
2022-06-13 22:30:42 +0300

    feat adds load static file

--- src/ex4/server.js
+++ src/ex4/server.js
@@ -2,6 +2,12 @@ import express from 'express'
 const port = 8080;
 const app = express();
 
+app.use(express.json())
+app.use(express.static('dist'))
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
 
 app.listen(port, () => {
     console.log("Server started on port", port);
