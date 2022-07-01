663452433b9cbd7f5e87bb7e150ce3a324dc57d4
Ouriel91 <orieloh91@gmail.com>
2022-07-01 12:03:27 +0300

    added build

--- src/project/server.js
+++ src/project/server.js
@@ -2,6 +2,7 @@ const express =  require('express')
 const todoRouter = require('./server/routes/api.js')
 const logger = require('./server/middlewares/logger.js')
 const cors = require('cors')
+const path = require('path')
 
 const port = 8080;
 const app = express();
@@ -9,7 +10,15 @@ const app = express();
 app.use([logger])
 app.use(express.json())
 app.use(cors())
+
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 app.use('/todo', todoRouter)
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 'OK'
+    })
+})
 
 app.listen(port, () => {
     console.log("Server started on port", port);
