67ed26e9e72b7e190422b06282e0c5f07dd507a3
Ouriel91 <orieloh91@gmail.com>
2022-07-01 11:18:15 +0300

    change style

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,38 +1,30 @@
-import './App.css';
-import Container from './components/App/Container/Container';
+import "./App.css";
+import Container from "./components/App/Container/Container";
 import { Tab, TabList } from "monday-ui-react-core";
-import "monday-ui-react-core/dist/main.css"
+import "monday-ui-react-core/dist/main.css";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import About from './components/other/About/Container';
-import Statistics from './components/other/Statistics/Statistics'
+import About from "./components/other/About/Container";
+import Statistics from "./components/other/Statistics/Statistics";
 
 function App() {
-  const navigate = useNavigate ();
+  const navigate = useNavigate();
 
   return (
-      <div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Container />} />
+        <Route path="about" element={<About />} />
+        <Route path="stats" element={<Statistics />} />
+      </Routes>
 
-        <Routes>
-          <Route path="/" element={<Container />} />
-          <Route path="about" element={<About />} />
-          <Route path="stats" element={<Statistics />} />
-        </Routes>
-
-        <div className="tabs">
-          <TabList>
-            <Tab onClick={() => navigate('/')}>
-              App
-            </Tab>
-            <Tab onClick={() => navigate('stats')}>
-              Statistics
-            </Tab>
-            <Tab onClick={() => navigate('about')}>
-              About
-            </Tab>
-          </TabList>
-        </div>
-          
+      <div className="tabs">
+        <TabList>
+          <Tab onClick={() => navigate("/")}>App</Tab>
+          <Tab onClick={() => navigate("stats")}>Statistics</Tab>
+          <Tab onClick={() => navigate("about")}>About</Tab>
+        </TabList>
       </div>
+    </div>
   );
 }
 
