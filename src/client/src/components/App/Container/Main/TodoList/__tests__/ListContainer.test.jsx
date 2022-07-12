d1daa2dbde4090e1ae6300ccb237cd099147acc2
Ouriel91 <orieloh91@gmail.com>
2022-07-12 11:12:02 +0300

    prepare for task

---
+++ src/client/src/components/App/Container/Main/TodoList/__tests__/ListContainer.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import ListContainer from "../TodoList";
+import { Provider } from "react-redux";
+import { store } from "../../../../../../state managment/store";
+
+const items = [
+  {
+    id: 56,
+    name: "Take dog out for a walk",
+    status: false,
+  },
+  {
+    id: 32,
+    name: "Do the dishes",
+    status: true,
+  },
+];
+
+describe("ListContainer", () => {
+  test("should render both items (one done and one not)", () => {
+    render(
+      <Provider store={store}>
+        <ListContainer items={items} fetchItems={jest.fn(() => items)} />
+      </Provider>
+    );
+
+    // TODO: test that both items are rendered at the list
+  });
+});
